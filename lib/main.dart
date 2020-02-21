c6a947e7711cf3f4e7e0a86d4c1ea1cbaa6bf00f
Haoran Yu <haoranyu6@gmail.com>
2020-02-21 14:39:54 -0800

    Add question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,10 +28,10 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionList = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true)
   ];
 
   int questionIndex = 0;
@@ -47,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionIndex],
+                questionList[questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +73,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAns = questionList[questionIndex].questionAns;
+
+                if (correctAns) {
+                  print('Correct');
+                } else {
+                  print('Wrong');
+                }
+
                 setState(() {
                   questionIndex++;
                 });
@@ -93,6 +102,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAns = questionList[questionIndex].questionAns;
+
+                if (!correctAns) {
+                  print('Correct');
+                } else {
+                  print('Wrong');
+                }
+
+                setState(() {
+                  questionIndex++;
+                });
               },
             ),
           ),
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool questionAns;
+
+  Question(String questionText, bool questionAns) {
+    this.questionText = questionText;
+    this.questionAns = questionAns;
+  }
+}

a9343abf99dbcd47b79b06ff400d0ab0d6611e66
Haoran Yu <haoranyu6@gmail.com>
2020-02-21 15:02:58 -0800

    Add quiz_lib class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_lib.dart';
+
+QuizLib quizLib = QuizLib();
 
 void main() => runApp(Quizzler());
 
@@ -28,14 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<Question> questionList = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true)
-  ];
-
-  int questionIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -48,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionList[questionIndex].questionText,
+                quizLib.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,7 +67,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAns = questionList[questionIndex].questionAns;
+                bool correctAns = quizLib.getQuestionAns();
 
                 if (correctAns) {
                   print('Correct');
@@ -82,7 +76,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  questionIndex++;
+                  quizLib.nextQuestion();
                 });
               },
             ),
@@ -102,7 +96,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAns = questionList[questionIndex].questionAns;
+                bool correctAns = quizLib.getQuestionAns();
 
                 if (!correctAns) {
                   print('Correct');
@@ -111,7 +105,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  questionIndex++;
+                  quizLib.nextQuestion();
                 });
               },
             ),
